6224e8e69b608064c4597bdfcbbc7f045c4c3a46
Johan Nordström <johan@digitalvillage.se>
2024-01-08 13:43:46 +0100

    Register request-handlers for specific routes and respond to incoming requests for those routes 😎.

--- 02-express/server.js
+++ 02-express/server.js
@@ -9,6 +9,17 @@ const PORT = 3000;
 // Create a new Express app
 const app = express();
 
+// Listen for incoming GET request to "/"
+app.get("/", (req, res) => {
+	console.log("Someone request to GET my (g)root");
+	res.send("Oh, hi there 😊");
+});
+
+// Listen for incoming GET request to "/coffee"
+app.get("/coffee", (req, res) => {
+	res.send("Is good for you!");
+});
+
 // Start listening for incoming requests on port 3000
 app.listen(PORT, () => {
 	// Will be invoked once the server has started listening
