375187a90b1791b19202dc6128764d44552aeec4
Johan Nordström <johan@digitalvillage.se>
2024-01-08 13:19:42 +0100

    Our very first (but a bit useless) Express app 🤩.

--- 02-express/server.js
+++ 02-express/server.js
@@ -1,3 +1,16 @@
 /**
  * Express Server
  */
+
+// Require express
+const express = require("express");
+const PORT = 3000;
+
+// Create a new Express app
+const app = express();
+
+// Start listening for incoming requests on port 3000
+app.listen(PORT, () => {
+	// Will be invoked once the server has started listening
+	console.log(`🥳 Yay, server started on localhost:${PORT}`);
+});
