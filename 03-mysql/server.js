7fe25a2dbded22ffffb323de6ac583b725f973e8
Johan Nordström <johan@digitalvillage.se>
2024-01-11 16:09:19 +0100

    Installed  and . Created connection to the database and made our first query 💪🏻.

--- 03-mysql/server.js
+++ 03-mysql/server.js
@@ -7,6 +7,23 @@ const express = require("express");
 const morgan = require("morgan");
 const PORT = 3000;
 
+// Read any .env-files
+require('dotenv').config();
+
+// console.log("DATABASE_HOST:", process.env.DATABASE_HOST);
+
+// Get the client
+const mysql = require("mysql2/promise");
+
+// Create the connection to the database
+const connection = mysql.createConnection({
+	host: process.env.DATABASE_HOST,
+	port: process.env.DATABASE_PORT,
+	user: process.env.DATABASE_USER,
+	password: process.env.DATABASE_PASSWORD,
+	database: process.env.DATABASE_NAME,
+});
+
 // Create a new Express app
 const app = express();
 
@@ -24,8 +41,19 @@ app.get("/", (req, res) => {
 });
 
 // GET all users
-app.get("/users", (req, res) => {
-	res.send(users);
+app.get("/users", async (req, res) => {
+	// Wait for connection to be established
+	const db = await connection;
+
+	// Execute a query
+	// const result = await db.query("SELECT * FROM users");
+
+	// Extract rows from result
+	// const rows = result[0];
+	const [rows] = await db.query("SELECT * FROM users");
+
+	// Respond with rows
+	res.send(rows);
 });
 
 // Catch any requests that does not have a matching handler
