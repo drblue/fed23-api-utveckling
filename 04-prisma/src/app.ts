07b4a015dfef4c2dc3c7f30aaf4b20ea72e85541
Johan Nordström <johan@digitalvillage.se>
2024-01-18 09:55:44 +0100

    Get all phones and users.

--- 04-prisma/src/app.ts
+++ 04-prisma/src/app.ts
@@ -21,6 +21,14 @@ app.get("/", (req, res) => {
  * Get all phones
  */
 app.get("/phones", async (req, res) => {
+	try {
+		const phones = await prisma.phones.findMany();
+		res.send(phones);
+
+	} catch (err) {
+		console.error(err);
+		res.status(500).send({ message: "Something went wrong when querying the database" });
+	}
 });
 
 /**
@@ -37,6 +45,14 @@ app.get("/phones/:phoneId", async (req, res) => {
  * Get all users
  */
 app.get("/users", async (req, res) => {
+	try {
+		const users = await prisma.users.findMany();
+		res.send(users);
+
+	} catch (err) {
+		console.error(err);
+		res.status(500).send({ message: "Something went wrong when querying the database" });
+	}
 });
 
 /**
