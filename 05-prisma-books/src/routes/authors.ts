05a00423f4824bebca02808bd70035a06c851368
Johan Nordström <johan@digitalvillage.se>
2024-01-22 13:59:58 +0100

    Moved Author- and Book-routes to their own modules that are then imported in `app.ts`.

---
+++ 05-prisma-books/src/routes/authors.ts
@@ -0,0 +1,128 @@
+import express from "express";
+import prisma from "../prisma";
+const router = express.Router();
+
+/**
+ * GET /authors
+ *
+ * Get all authors
+ */
+router.get("/authors", async (req, res) => {
+	try {
+		const authors = await prisma.author.findMany();
+		res.send(authors);
+
+	} catch (err) {
+		console.error(err);
+		res.status(500).send({ message: "Something went wrong when querying the database" });
+	}
+});
+
+/**
+ * GET /authors/:authorId
+ *
+ * Get a single author
+ */
+router.get("/authors/:authorId", async (req, res) => {
+	const authorId = Number(req.params.authorId);
+
+	try {
+		const author = await prisma.author.findUniqueOrThrow({
+			where: {
+				id: authorId,
+			},
+			include: {
+				books: true,
+			},
+		});
+		res.send(author);
+
+	} catch (err: any) {
+		if (err.code === "P2025") {
+			// NotFoundError
+			console.log(err);
+			res.status(404).send({ message: "Author Not Found" });
+		} else {
+			console.error(err);
+			res.status(500).send({ message: "Something went wrong when querying the database" });
+		}
+
+	}
+});
+
+/**
+ * POST /authors
+ *
+ * Create a author
+ */
+router.post("/authors", async (req, res) => {
+	try {
+		const author = await prisma.author.create({
+			data: req.body,
+		});
+		res.status(201).send(author);
+
+	} catch (err) {
+		console.error(err);
+		res.status(500).send({ message: "Something went wrong when creating the record in the database" });
+	}
+});
+
+/**
+ * PATCH /authors/:authorId
+ *
+ * Update a author
+ */
+router.patch("/authors/:authorId", async (req, res) => {
+	const authorId = Number(req.params.authorId);
+
+	try {
+		const author = await prisma.author.update({
+			where: {
+				id: authorId,
+			},
+			data: req.body,
+		});
+		res.status(200).send(author);
+
+	} catch (err: any) {
+		if (err.code === "P2025") {
+			// NotFoundError
+			console.log(err);
+			res.status(404).send({ message: "Author Not Found" });
+		} else {
+			console.error(err);
+			res.status(500).send({ message: "Something went wrong when querying the database" });
+		}
+	}
+});
+
+/**
+ * DELETE /authors/:authorId
+ *
+ * Delete a author
+ */
+router.delete("/authors/:authorId", async (req, res) => {
+	const authorId = Number(req.params.authorId);
+
+	try {
+		await prisma.author.delete({
+			where: {
+				id: authorId,
+			}
+		});
+		res.status(200).send({});
+
+	} catch (err: any) {
+		if (err.code === "P2025") {
+			// NotFoundError
+			console.log(err);
+			res.status(404).send({ message: "Author Not Found" });
+		} else {
+			console.error(err);
+			res.status(500).send({ message: "Something went wrong when querying the database" });
+		}
+	}
+});
+
+export default router;
---
+++ 05-prisma-books/src/routes/books.ts
@@ -0,0 +1,203 @@
+import express from "express";
+import prisma from "../prisma";
+const router = express.Router();
+
+/**
+ * GET /books
+ *
+ * Get all books
+ */
+router.get("/books", async (req, res) => {
+	try {
+		const books = await prisma.book.findMany();
+		res.send(books);
+
+	} catch (err) {
+		console.error(err);
+		res.status(500).send({ message: "Something went wrong when querying the database" });
+	}
+});
+
+/**
+ * GET /books/:bookId
+ *
+ * Get a single book
+ */
+router.get("/books/:bookId", async (req, res) => {
+	const bookId = Number(req.params.bookId);
+
+	try {
+		const book = await prisma.book.findUniqueOrThrow({
+			where: {
+				id: bookId,
+			},
+			include: {
+				authors: true,
+			},
+		});
+		res.send(book);
+
+	} catch (err: any) {
+		if (err.code === "P2025") {
+			// NotFoundError
+			console.log(err);
+			res.status(404).send({ message: "Book Not Found" });
+		} else {
+			console.error(err);
+			res.status(500).send({ message: "Something went wrong when querying the database" });
+		}
+
+	}
+});
+
+/**
+ * POST /books
+ *
+ * Create a book
+ */
+router.post("/books", async (req, res) => {
+	try {
+		const book = await prisma.book.create({
+			data: req.body,
+		});
+		res.status(201).send(book);
+
+	} catch (err) {
+		console.error(err);
+		res.status(500).send({ message: "Something went wrong when creating the record in the database" });
+	}
+});
+
+/**
+ * PATCH /books/:bookId
+ *
+ * Update a book
+ */
+router.patch("/books/:bookId", async (req, res) => {
+	const bookId = Number(req.params.bookId);
+
+	try {
+		const book = await prisma.book.update({
+			where: {
+				id: bookId,
+			},
+			data: req.body,
+		});
+		res.status(200).send(book);
+
+	} catch (err: any) {
+		if (err.code === "P2025") {
+			// NotFoundError
+			console.log(err);
+			res.status(404).send({ message: "Book Not Found" });
+		} else {
+			console.error(err);
+			res.status(500).send({ message: "Something went wrong when querying the database" });
+		}
+	}
+});
+
+/**
+ * DELETE /books/:bookId
+ *
+ * Delete a book
+ */
+router.delete("/books/:bookId", async (req, res) => {
+	const bookId = Number(req.params.bookId);
+
+	try {
+		await prisma.book.delete({
+			where: {
+				id: bookId,
+			}
+		});
+		res.status(200).send({});
+
+	} catch (err: any) {
+		if (err.code === "P2025") {
+			// NotFoundError
+			console.log(err);
+			res.status(404).send({ message: "Book Not Found" });
+		} else {
+			console.error(err);
+			res.status(500).send({ message: "Something went wrong when querying the database" });
+		}
+	}
+});
+
+/**
+ * POST /books/:bookId/authors
+ *
+ * Link book to author(s)
+ */
+router.post("/books/:bookId/authors", async (req, res) => {
+	const bookId = Number(req.params.bookId);
+
+	try {
+		const book = await prisma.book.update({
+			where: {
+				id: bookId,
+			},
+			data: {
+				authors: {
+					connect: req.body,  // { "id": 7 }
+				},
+			},
+			include: {
+				authors: true,
+			}
+		});
+		res.status(201).send(book);
+
+	} catch (err: any) {
+		if (err.code === "P2025") {
+			// NotFoundError
+			console.log(err);
+			res.status(404).send({ message: "Book or Author Not Found" });
+		} else {
+			console.error(err);
+			res.status(500).send({ message: "Something went wrong when querying the database" });
+		}
+	}
+});
+
+/**
+ * DELETE /books/:bookId/authors/:authorId
+ *
+ * Unlink an author from a book
+ */
+router.delete("/books/:bookId/authors/:authorId", async (req, res) => {
+	const bookId = Number(req.params.bookId);
+	const authorId = Number(req.params.authorId);
+
+	try {
+		const book = await prisma.book.update({
+			where: {
+				id: bookId,
+			},
+			data: {
+				authors: {
+					disconnect: {
+						id: authorId,
+					},
+				},
+			},
+			include: {
+				authors: true,
+			}
+		});
+		res.status(200).send(book);
+
+	} catch (err: any) {
+		if (err.code === "P2025") {
+			// NotFoundError
+			console.log(err);
+			res.status(404).send({ message: "Book Not Found" });
+		} else {
+			console.error(err);
+			res.status(500).send({ message: "Something went wrong when querying the database" });
+		}
+	}
+});
+
+export default router;
