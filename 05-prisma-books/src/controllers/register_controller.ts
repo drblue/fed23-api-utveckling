e306cbada67d910a2869e5153aadd9b954572e70
Johan Nordström <johan@digitalvillage.se>
2024-01-25 14:57:44 +0100

    Validate incoming register-data and throw validation errors. Also get only the validated data.

--- 05-prisma-books/src/controllers/register_controller.ts
+++ 05-prisma-books/src/controllers/register_controller.ts
@@ -1,7 +1,12 @@
 /**
  * Register Controller
  */
+import Debug from "debug";
 import { Request, Response } from "express";
+import { matchedData, validationResult } from "express-validator";
+
+// Create a new debug instance
+const debug = Debug("prisma-books:register_controller");
 
 /**
  * Register a new user
@@ -10,6 +15,19 @@ import { Request, Response } from "express";
  */
 export const register = async (req: Request, res: Response) => {
 	// Validate incoming data
+	const validationErrors = validationResult(req);
+	if (!validationErrors.isEmpty()) {
+		res.status(400).send({
+			status: "fail",
+			data: validationErrors.array(),
+		});
+		return;
+	}
+
+	// Get only the validated data from the request
+	const validatedData = matchedData(req);
+	debug("req.body: %O", req.body);
+	debug("validatedData: %O", validatedData);
 
 	// Calculate a hash + salt for the password
 
