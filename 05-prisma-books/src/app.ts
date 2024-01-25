d496b1d52e35af920e2ece921be56d0a47c97dce
Johan Nordström <johan@digitalvillage.se>
2024-01-25 10:26:13 +0100

    Added support for CORS.

--- 05-prisma-books/src/app.ts
+++ 05-prisma-books/src/app.ts
@@ -1,8 +1,10 @@
+import cors from "cors";
 import express from "express";
 import morgan from "morgan";
 import routes from "./routes";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
