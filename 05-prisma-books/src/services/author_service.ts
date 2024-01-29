d95ac2e3a35246666f4ab92118d5d2bcb29aa2cb
Johan Nordström <johan@digitalvillage.se>
2024-01-29 11:25:44 +0100

    Added Author service skeleton 🩻.

---
+++ 05-prisma-books/src/services/author_service.ts
@@ -0,0 +1,26 @@
+/**
+ * Author Service
+ */
+import prisma from "../prisma";
+
+/**
+ * Get all authors
+ */
+export const getAuthors = async () => {
+}
+
+/**
+ * Get a single author
+ *
+ * @param authorId The id of the author to get
+ */
+export const getAuthor = async (authorId: number) => {
+}
+
+/**
+ * Delete an author
+ *
+ * @param authorId The id of the author to delete
+ */
+export const deleteAuthor = async (authorId: number) => {
+}
