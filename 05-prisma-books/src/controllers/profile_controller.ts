3cba0c17555057746182e81c300c63da13750b3b
Johan Nordström <johan@digitalvillage.se>
2024-01-31 14:09:59 +0100

    Send the user's details back to them on `GET /profile`.

--- 05-prisma-books/src/controllers/profile_controller.ts
+++ 05-prisma-books/src/controllers/profile_controller.ts
@@ -1,15 +1,29 @@
 /**
  * Profile Controller
  */
+import Debug from "debug";
 import { Request, Response } from "express";
+
+// Create a new debug instance
+const debug = Debug("prisma-books:profile_controller");
 
 /**
  * Get the authenticated user's profile
  */
 export const getProfile = async (req: Request, res: Response) => {
+	// If someone ever removes authentication from the route for this method, yell at them
+	if (!req.user) {
+		throw new Error("Trying to access autenticated user but none exists. Did you remove autentication from this route? 🤬");
+	}
+
+	// Respond with ID 🪪
 	res.send({
 		status: "success",
-		data: null,
+		data: {
+			id: req.user.id,
+			name: req.user.name,
+			email: req.user.email,
+		},
 	});
 }
 
@@ -19,6 +33,6 @@ export const getProfile = async (req: Request, res: Response) => {
 export const getBooks = async (req: Request, res: Response) => {
 	res.send({
 		status: "success",
-		data: null,
+		data: [],
 	});
 }
