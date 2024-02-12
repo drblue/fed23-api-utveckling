7dd7bc79d628b1b1f2bd0f3a1be427d67bfdfebb
Johan Nordström <johan@digitalvillage.se>
2024-02-12 13:59:22 +0100

    Added workshop-instructions.

--- 06-lmdb/src/resources/movie/movie.controller.ts
+++ 06-lmdb/src/resources/movie/movie.controller.ts
@@ -28,3 +28,33 @@ export const index = async (req: Request, res: Response) => {
 		});
 	}
 }
+
+/**
+ * Get a single movie
+ */
+export const show = async (req: Request, res: Response) => {
+	const movieId = req.params.movieId;
+
+	try {
+		// Find a single movie
+		const movie = await Movie.findById(movieId);
+
+		// If no movie was found, report 404
+		if (!movie) {
+			res.status(404).send({ status: "fail", message: "No such movie exists" });
+			return;
+		}
+
+		res.send({
+			status: "success",
+			data: movie,
+		});
+
+	} catch (err) {
+		debug("Error thrown when finding movie", err);
+		res.status(500).send({
+			status: "error",
+			message: "Error thrown when finding movie",
+		});
+	}
+}
--- 06-lmdb/src/resources/movie/movie.router.ts
+++ 06-lmdb/src/resources/movie/movie.router.ts
@@ -7,4 +7,9 @@ const router = express.Router();
  */
 router.get("/", movieController.index);
 
+/**
+ * GET /movies/:movieId
+ */
+router.get("/:movieId", movieController.show);
+
 export default router;
