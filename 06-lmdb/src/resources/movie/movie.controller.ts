65b78d5ae9d08a7860814b11db463da1d90fbf11
Johan Nordström <johan@digitalvillage.se>
2024-02-14 11:47:30 +0100

    Populate `director` attribute when retrieving a single `Movie`.

--- 06-lmdb/src/resources/movie/movie.controller.ts
+++ 06-lmdb/src/resources/movie/movie.controller.ts
@@ -38,7 +38,7 @@ export const show = async (req: Request, res: Response) => {
 
 	try {
 		// Find a single movie
-		const movie = await Movie.findById(movieId);
+		const movie = await Movie.findById(movieId).populate("director", "name");
 
 		// If no movie was found, report 404
 		if (!movie) {
