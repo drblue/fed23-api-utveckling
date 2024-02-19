2a95594889c23e233759fa3dd1d2edf09d6420f2
Johan Nordström <johan@digitalvillage.se>
2024-02-19 11:08:26 +0100

    Created our first, very rudementary Socket.IO server.

--- 07-chat/backend/server.ts
+++ 07-chat/backend/server.ts
@@ -1,6 +1,7 @@
 import app from "./src/app";
 import http from "http";
 import * as dotenv from "dotenv";
+import { Server } from "socket.io";
 
 // Initialize dotenv so it reads our `.env`-file
 dotenv.config();
@@ -9,19 +10,33 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 /**
- * Create HTTP server.
+ * Create HTTP and Socket.IO server.
  */
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+	cors: {
+		origin: "*",
+		credentials: true,
+	}
+});
+
+/**
+ * Handle incoming Socket.IO connection
+ */
+io.on("connection", (socket) => {           // btn.addEventListener("click", (e) => {})
+	// Yay someone connected to me
+	console.log("Yayy someone connected!!!!", socket.id);
+});
 
 /**
  * Listen on provided port, on all network interfaces.
  */
-server.listen(PORT);
+httpServer.listen(PORT);
 
 /**
  * Event listener for HTTP server "error" event.
  */
-server.on("error", (err: NodeJS.ErrnoException) => {
+httpServer.on("error", (err: NodeJS.ErrnoException) => {
 	if (err.syscall !== "listen") {
 		throw err;
 	}
@@ -43,6 +58,6 @@ server.on("error", (err: NodeJS.ErrnoException) => {
 /**
  * Event listener for HTTP server "listening" event.
  */
-server.on("listening", () => {
+httpServer.on("listening", () => {
 	console.log(`🚀 Yay, server started on http://localhost:${PORT}`);
 });
