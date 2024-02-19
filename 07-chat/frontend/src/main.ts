a4c693e6b1f7635d93260daf2918c305ef6abafc
Johan Nordström <johan@digitalvillage.se>
2024-02-19 12:03:46 +0100

    Connect to our Socket.IO server. Also listen for when a connection is established.

--- 07-chat/frontend/src/main.ts
+++ 07-chat/frontend/src/main.ts
@@ -1,5 +1,17 @@
+import { io } from "socket.io-client";
 import "./assets/scss/style.scss";
+
+const SOCKET_HOST = import.meta.env.VITE_SOCKET_HOST;
+console.log("SOCKET_HOST:", SOCKET_HOST);
 
 const messageEl = document.querySelector("#message") as HTMLInputElement;
 const messageFormEl = document.querySelector("#message-form") as HTMLFormElement;
 const messagesEl = document.querySelector("#messages") as HTMLDivElement;
+
+// Connect to Socket.IO Server
+const socket = io(SOCKET_HOST);
+
+// Listen for when connection is established
+socket.on("connect", () => {
+	console.log("💥 Connected to the server", socket.id);
+});
