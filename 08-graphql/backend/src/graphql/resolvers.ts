d0ee967f824bf85374e41276c6f7f7ed26892e9e
Johan Nordström <johan@digitalvillage.se>
2024-03-04 11:48:47 +0100

    Added type-definitions for Author and Publisher, as well as resolvers for authors and publishers queries.

--- 08-graphql/backend/src/graphql/resolvers.ts
+++ 08-graphql/backend/src/graphql/resolvers.ts
@@ -1,6 +1,18 @@
 // Resolvers define how to fetch the types defined in your schema.
+import prisma from "../prisma";
 
 const resolvers = {
+	Query: {
+		authors: () => {
+			return prisma.author.findMany();
+		},
+		books: () => {
+			return prisma.book.findMany();
+		},
+		publishers: () => {
+			return prisma.publisher.findMany();
+		},
+	},
 };
 
 export default resolvers;
--- 08-graphql/backend/src/graphql/typeDefs.ts
+++ 08-graphql/backend/src/graphql/typeDefs.ts
@@ -5,6 +5,12 @@
 const typeDefs = `#graphql
 	# Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
+	type Author {
+		id: Int!
+		name: String!
+		birthyear: Int
+	}
+
 	# This "Book" type defines the queryable fields for every book in our data source.
 	type Book {
 		id: Int!
@@ -13,11 +19,18 @@ const typeDefs = `#graphql
 		publisherId: Int
 	}
 
+	type Publisher {
+		id: Int!
+		name: String!
+	}
+
 	# The "Query" type is special: it lists all of the available queries that
 	# clients can execute, along with the return type for each. In this
 	# case, the "books" query returns an array of zero or more Books (defined above).
 	type Query {
+		authors: [Author!]
 		books: [Book!]
+		publishers: [Publisher!]
 	}
 `;
 
