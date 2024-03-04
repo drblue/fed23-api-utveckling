386f2d4516663cd36bed53948209b58b916d2c7a
Johan Nordström <johan@digitalvillage.se>
2024-03-04 11:28:26 +0100

    Created our first GraphQL-server, a type-definition for a Book and the first Query.

--- 08-graphql/backend/src/graphql/typeDefs.ts
+++ 08-graphql/backend/src/graphql/typeDefs.ts
@@ -7,12 +7,17 @@ const typeDefs = `#graphql
 
 	# This "Book" type defines the queryable fields for every book in our data source.
 	type Book {
+		id: Int!
+		title: String!
+		pages: Int!
+		publisherId: Int
 	}
 
 	# The "Query" type is special: it lists all of the available queries that
 	# clients can execute, along with the return type for each. In this
 	# case, the "books" query returns an array of zero or more Books (defined above).
 	type Query {
+		books: [Book!]
 	}
 `;
 
--- 08-graphql/backend/src/server.ts
+++ 08-graphql/backend/src/server.ts
@@ -1,7 +1,29 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 import * as dotenv from "dotenv";
+import resolvers from "./graphql/resolvers";
+import typeDefs from "./graphql/typeDefs";
 
 // Initialize dotenv so it reads our `.env`-file
 dotenv.config();
 
 // Read port to start server on from `.env`, otherwise default to port 4000
 const PORT = Number(process.env.PORT) || 4000;
+
+// Set up the Apollo Server
+const server = new ApolloServer({
+	typeDefs,  // typeDefs: typeDefs
+	resolvers,
+});
+
+// Start a standalone server on the specified port
+startStandaloneServer(server, {
+	listen: {
+		port: PORT,
+	}
+}).then((props) => {
+	console.log(`🚀 Server ready at: ${props.url}`);
+}).catch(err => {
+	console.error(`😱 Could not start server because:`, err);
+	process.exit(1);
+});
